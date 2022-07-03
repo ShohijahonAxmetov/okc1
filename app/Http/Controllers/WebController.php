014ef7faaa1ed94702d9f4933c6325d16ac40459
shohijahonaxmetov <shohijahonaxmetov@gmail.com>
2022-07-03 21:16:32 +0500

    order fixed

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -124,6 +124,7 @@ class WebController extends Controller
     public function order(Request $request)
     {
     	$data = $request->all();
+        dd($data);
 
         // validaciya dannix
         $validator = Validator::make($data, [
