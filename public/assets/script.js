ca7fc932d425e81c3e7c9e1260ca00c2c17e065d
ShohijahonAxmetov <shohijahonaxmetov@gmail.com>
2022-07-18 18:06:06 +0500

    download updates btn animation

---
+++ public/assets/script.js
@@ -0,0 +1,8 @@
+let button = document.getElementById('download_updates')
+let circle = document.getElementById('circle')
+let btnTxt = document.getElementById('btn-txt')
+
+function clickBtn(){
+    button.classList.add('clicked')
+    circle.classList.add('rotation')
+}
