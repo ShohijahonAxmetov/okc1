15f021ea2865af3a3449189e212be4f812dcfa73
ShohijahonAxmetov <shohijahonaxmetov@gmail.com>
2022-07-23 18:14:26 +0500

    profile page end

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Admin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Str;
 
 class ProfileController extends Controller
 {
@@ -18,8 +20,43 @@ class ProfileController extends Controller
 
     public function update(Request $request)
     {
-        $user = auth()->user();
+        if ($request->password != $request->password_confirmation) {
+            return back()->with([
+                'success' => false,
+                'message' => 'Пароли не совпадают'
+            ]);
+        }
 
-        dd($user);
+        $request->validate([
+            'name' => 'required|max:255',
+            'password' => 'required|confirmed|min:8|max:16',
+            'avatar' => 'image|max:2048'
+        ]);
+
+        $user = auth()->user();
+        $avatar = $user->img;
+
+        if ($request->hasFile('avatar')) {
+            $img = $request->file('avatar');
+            $img_name = Str::random(12) . '.' . $img->extension();
+            $saved_img = $img->move(public_path('/upload/admins'), $img_name);
+            Image::make($saved_img)->resize(200, null, function ($constraint) {
+                $constraint->aspectRatio();
+            })->save(public_path() . '/upload/admins/200/' . $img_name, 60);
+
+            $avatar = $img_name;
+        }
+
+        $user->update([
+            'name' => $request->name,
+            'phone_number' => $request->phone_number,
+            'password' => Hash::make($request->password),
+            'img' => $avatar
+        ]);
+
+        return back()->with([
+            'success' => true,
+            'message' => 'Успешно редактирован'
+        ]);;
     }
 }
--- resources/views/app/profile.blade.php
+++ resources/views/app/profile.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 
-@section('title', 'PROFILE')
+@section('title', 'Профиль')
 
 @section('breadcrumb')
 
 @include('app.components.breadcrumb', [
 'items' => [
 [
-'title' => 'home',
+'title' => 'главная',
 'route' => 'dashboard'
 ],
 [
-'title' => 'profile'
+'title' => 'Профиль'
 ]
 ]
 ])
@@ -24,7 +24,7 @@
     <!--begin::Header-->
     <div class="card-header border-0 pt-5">
         <h3 class="card-title align-items-start flex-column">
-            <span class="card-label fw-bolder fs-3 mb-1">Profile Details</span>
+            <span class="card-label fw-bolder fs-3 mb-1">Редактировать профиль</span>
         </h3>
         <div class="card-toolbar">
         </div>
@@ -34,21 +34,23 @@
     <div class="card-body py-3">
         <div>
             <!--begin::Form-->
-            <form action="{{ route('profile.update') }}" method="post" enctype="multipart/form-data" id="kt_account_profile_details_form" class="form">
+            <form action="{{ route('profile.update') }}" method="post" enctype="multipart/form-data" class="form">
                 @csrf
                 <!--begin::Card body-->
                 <div class="card-body border-top p-9">
                     <!--begin::Input group-->
                     <div class="row mb-6">
                         <!--begin::Label-->
-                        <label class="col-lg-4 col-form-label fw-bold fs-6">Avatar</label>
+                        <label class="col-lg-4 col-form-label fw-bold fs-6">Аватарка</label>
                         <!--end::Label-->
                         <!--begin::Col-->
                         <div class="col-lg-8">
                             <!--begin::Image input-->
                             <div class="image-input image-input-outline" data-kt-image-input="true" style="background-image: url('/assets/media/svg/avatars/blank.svg')">
                                 <!--begin::Preview existing avatar-->
-                                <div class="image-input-wrapper w-125px h-125px" style="background-image: url(/assets/media/avatars/300-1.jpg)"></div>
+                                @if($user->img)
+                                <div class="image-input-wrapper w-125px h-125px" style="background-image: url({{ $user->min_img }})"></div>
+                                @endif
                                 <!--end::Preview existing avatar-->
                                 <!--begin::Label-->
                                 <label class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="change" data-bs-toggle="tooltip" title="Change avatar">
@@ -65,14 +67,14 @@
                                 </span>
                                 <!--end::Cancel-->
                                 <!--begin::Remove-->
-                                <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="remove" data-bs-toggle="tooltip" title="Remove avatar">
+                                <!-- <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="remove" data-bs-toggle="tooltip" title="Remove avatar">
                                     <i class="bi bi-x fs-2"></i>
-                                </span>
+                                </span> -->
                                 <!--end::Remove-->
                             </div>
                             <!--end::Image input-->
                             <!--begin::Hint-->
-                            <div class="form-text">Allowed file types: png, jpg, jpeg.</div>
+                            <div class="form-text">Разрешенные типы файлов: png, jpg, jpeg.</div>
                             <!--end::Hint-->
                         </div>
                         <!--end::Col-->
@@ -81,36 +83,12 @@
                     <!--begin::Input group-->
                     <div class="row mb-6">
                         <!--begin::Label-->
-                        <label class="col-lg-4 col-form-label required fw-bold fs-6">Full Name</label>
+                        <label class="col-lg-4 col-form-label required fw-bold fs-6">Ф.И.О.</label>
                         <!--end::Label-->
                         <!--begin::Col-->
                         <div class="col-lg-8">
-                            <!--begin::Row-->
-                            <div class="row">
-                                <!--begin::Col-->
-                                <div class="col-lg-6 fv-row">
-                                    <input type="text" name="fname" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" placeholder="First name" value="Max" />
-                                </div>
-                                <!--end::Col-->
-                                <!--begin::Col-->
-                                <div class="col-lg-6 fv-row">
-                                    <input type="text" name="lname" class="form-control form-control-lg form-control-solid" placeholder="Last name" value="Smith" />
-                                </div>
-                                <!--end::Col-->
-                            </div>
-                            <!--end::Row-->
-                        </div>
-                        <!--end::Col-->
-                    </div>
-                    <!--end::Input group-->
-                    <!--begin::Input group-->
-                    <div class="row mb-6">
-                        <!--begin::Label-->
-                        <label class="col-lg-4 col-form-label required fw-bold fs-6">Company</label>
-                        <!--end::Label-->
-                        <!--begin::Col-->
-                        <div class="col-lg-8 fv-row">
-                            <input type="text" name="company" class="form-control form-control-lg form-control-solid" placeholder="Company name" value="Keenthemes" />
+                            <input type="text" name="name" class="form-control form-control-lg form-control-solid mb-3 mb-lg-0" placeholder="Enter full name" value="{{ $user->name ?? old('name') }}" />
+
                         </div>
                         <!--end::Col-->
                     </div>
@@ -119,13 +97,13 @@
                     <div class="row mb-6">
                         <!--begin::Label-->
                         <label class="col-lg-4 col-form-label fw-bold fs-6">
-                            <span class="required">Contact Phone</span>
+                            <span class="required">Номер телефона</span>
                             <i class="fas fa-exclamation-circle ms-1 fs-7" data-bs-toggle="tooltip" title="Phone number must be active"></i>
                         </label>
                         <!--end::Label-->
                         <!--begin::Col-->
                         <div class="col-lg-8 fv-row">
-                            <input type="tel" name="phone" class="form-control form-control-lg form-control-solid" placeholder="Phone number" value="044 3276 454 935" />
+                            <input type="tel" name="phone_number" class="form-control form-control-lg form-control-solid" placeholder="+998 ..." value="{{ $user->phone_number ?? old('phone_number') }}" />
                         </div>
                         <!--end::Col-->
                     </div>
@@ -133,11 +111,11 @@
                     <!--begin::Input group-->
                     <div class="row mb-6">
                         <!--begin::Label-->
-                        <label class="col-lg-4 col-form-label fw-bold fs-6">Company Site</label>
+                        <label class="col-lg-4 col-form-label fw-bold fs-6">Пароль</label>
                         <!--end::Label-->
                         <!--begin::Col-->
                         <div class="col-lg-8 fv-row">
-                            <input type="text" name="website" class="form-control form-control-lg form-control-solid" placeholder="Company website" value="keenthemes.com" />
+                            <input type="password" name="password" minlength="8" required class="form-control form-control-lg form-control-solid" />
                         </div>
                         <!--end::Col-->
                     </div>
@@ -145,138 +123,20 @@
                     <!--begin::Input group-->
                     <div class="row mb-6">
                         <!--begin::Label-->
-                        <label class="col-lg-4 col-form-label fw-bold fs-6">
-                            <span class="required">Country</span>
-                            <i class="fas fa-exclamation-circle ms-1 fs-7" data-bs-toggle="tooltip" title="Country of origination"></i>
-                        </label>
+                        <label class="col-lg-4 col-form-label fw-bold fs-6">Подтверждение пароля</label>
                         <!--end::Label-->
                         <!--begin::Col-->
                         <div class="col-lg-8 fv-row">
-                            <select name="country" aria-label="Select a Country" data-control="select2" data-placeholder="Select a country..." class="form-select form-select-solid form-select-lg fw-bold">
-                                <option value="">Select a Country...</option>
-                                <option data-kt-flag="flags/afghanistan.svg" value="AF">Afghanistan</option>
-                            </select>
+                            <input type="password" name="password_confirmation" minlength="8" required class="form-control form-control-lg form-control-solid"/>
                         </div>
                         <!--end::Col-->
-                    </div>
-                    <!--end::Input group-->
-                    <!--begin::Input group-->
-                    <div class="row mb-6">
-                        <!--begin::Label-->
-                        <label class="col-lg-4 col-form-label required fw-bold fs-6">Language</label>
-                        <!--end::Label-->
-                        <!--begin::Col-->
-                        <div class="col-lg-8 fv-row">
-                            <!--begin::Input-->
-                            <select name="language" aria-label="Select a Language" data-control="select2" data-placeholder="Select a language..." class="form-select form-select-solid form-select-lg">
-                                <option value="">Select a Language...</option>
-                                <option data-kt-flag="flags/indonesia.svg" value="id">Bahasa Indonesia - Indonesian</option>
-                            </select>
-                            <!--end::Input-->
-                            <!--begin::Hint-->
-                            <div class="form-text">Please select a preferred language, including date, time, and number formatting.</div>
-                            <!--end::Hint-->
-                        </div>
-                        <!--end::Col-->
-                    </div>
-                    <!--end::Input group-->
-                    <!--begin::Input group-->
-                    <div class="row mb-6">
-                        <!--begin::Label-->
-                        <label class="col-lg-4 col-form-label required fw-bold fs-6">Time Zone</label>
-                        <!--end::Label-->
-                        <!--begin::Col-->
-                        <div class="col-lg-8 fv-row">
-                            <select name="timezone" aria-label="Select a Timezone" data-control="select2" data-placeholder="Select a timezone.." class="form-select form-select-solid form-select-lg">
-                                <option value="">Select a Timezone..</option>
-                                <option data-bs-offset="-39600" value="International Date Line West">(GMT-11:00) International Date Line West</option>
-                            </select>
-                        </div>
-                        <!--end::Col-->
-                    </div>
-                    <!--end::Input group-->
-                    <!--begin::Input group-->
-                    <div class="row mb-6">
-                        <!--begin::Label-->
-                        <label class="col-lg-4 col-form-label fw-bold fs-6">Currency</label>
-                        <!--end::Label-->
-                        <!--begin::Col-->
-                        <div class="col-lg-8 fv-row">
-                            <select name="currnecy" aria-label="Select a Currency" data-control="select2" data-placeholder="Select a currency.." class="form-select form-select-solid form-select-lg">
-                                <option value="">Select a currency..</option>
-                                <option data-kt-flag="flags/united-states.svg" value="USD">
-                                    <b>USD</b>&#160;-&#160;USA dollar
-                                </option>
-                                <option data-kt-flag="flags/united-kingdom.svg" value="GBP">
-                                    <b>GBP</b>&#160;-&#160;British pound
-                                </option>
-                                <option data-kt-flag="flags/australia.svg" value="AUD">
-                                    <b>AUD</b>&#160;-&#160;Australian dollar
-                                </option>
-                                <option data-kt-flag="flags/japan.svg" value="JPY">
-                                    <b>JPY</b>&#160;-&#160;Japanese yen
-                                </option>
-                                <option data-kt-flag="flags/sweden.svg" value="SEK">
-                                    <b>SEK</b>&#160;-&#160;Swedish krona
-                                </option>
-                                <option data-kt-flag="flags/canada.svg" value="CAD">
-                                    <b>CAD</b>&#160;-&#160;Canadian dollar
-                                </option>
-                                <option data-kt-flag="flags/switzerland.svg" value="CHF">
-                                    <b>CHF</b>&#160;-&#160;Swiss franc
-                                </option>
-                            </select>
-                        </div>
-                        <!--end::Col-->
-                    </div>
-                    <!--end::Input group-->
-                    <!--begin::Input group-->
-                    <div class="row mb-6">
-                        <!--begin::Label-->
-                        <label class="col-lg-4 col-form-label required fw-bold fs-6">Communication</label>
-                        <!--end::Label-->
-                        <!--begin::Col-->
-                        <div class="col-lg-8 fv-row">
-                            <!--begin::Options-->
-                            <div class="d-flex align-items-center mt-3">
-                                <!--begin::Option-->
-                                <label class="form-check form-check-inline form-check-solid me-5">
-                                    <input class="form-check-input" name="communication[]" type="checkbox" value="1" />
-                                    <span class="fw-bold ps-2 fs-6">Email</span>
-                                </label>
-                                <!--end::Option-->
-                                <!--begin::Option-->
-                                <label class="form-check form-check-inline form-check-solid">
-                                    <input class="form-check-input" name="communication[]" type="checkbox" value="2" />
-                                    <span class="fw-bold ps-2 fs-6">Phone</span>
-                                </label>
-                                <!--end::Option-->
-                            </div>
-                            <!--end::Options-->
-                        </div>
-                        <!--end::Col-->
-                    </div>
-                    <!--end::Input group-->
-                    <!--begin::Input group-->
-                    <div class="row mb-0">
-                        <!--begin::Label-->
-                        <label class="col-lg-4 col-form-label fw-bold fs-6">Allow Marketing</label>
-                        <!--begin::Label-->
-                        <!--begin::Label-->
-                        <div class="col-lg-8 d-flex align-items-center">
-                            <div class="form-check form-check-solid form-switch fv-row">
-                                <input class="form-check-input w-45px h-30px" type="checkbox" id="allowmarketing" checked="checked" />
-                                <label class="form-check-label" for="allowmarketing"></label>
-                            </div>
-                        </div>
-                        <!--begin::Label-->
                     </div>
                     <!--end::Input group-->
                 </div>
                 <!--end::Card body-->
                 <!--begin::Actions-->
                 <div class="card-footer d-flex justify-content-end py-6 px-9">
-                    <button type="submit" class="btn btn-primary">Save Changes</button>
+                    <button type="submit" class="btn btn-primary">Сохранить</button>
                 </div>
                 <!--end::Actions-->
             </form>
